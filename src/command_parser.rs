95a8de8eae1334ec18146b44bb14cba9a9e1a8ff
Peter Arato <it.arato@gmail.com>
2025-11-24 21:44:17 -0500

    Case insensitive commands.

--- src/command_parser.rs
+++ src/command_parser.rs
@@ -11,9 +11,9 @@ impl CommandParser {
                 }
 
                 if let Some(name) = items[0].as_string() {
-                    if name == "PING" && items.len() == 1 {
+                    if name.to_lowercase() == "ping" && items.len() == 1 {
                         return Some(Command::Ping);
-                    } else if name == "ECHO" && items.len() == 2 {
+                    } else if name.to_lowercase() == "echo" && items.len() == 2 {
                         return Some(Command::Echo(items[1].clone()));
                     }
                 }
