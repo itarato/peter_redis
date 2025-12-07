2105f6fc4235e8720884c8b3570fa48b22ac8411
Peter Arato <it.arato@gmail.com>
2025-12-06 20:36:43 -0500

    Refactor network part.

---
+++ src/network.rs
@@ -0,0 +1,144 @@
+use anyhow::Context;
+use tokio::{
+    io::{AsyncBufReadExt, AsyncReadExt, BufReader},
+    net::TcpStream,
+};
+
+use crate::{common::Error, resp::RespValue};
+
+pub(crate) struct StreamReader<'a> {
+    buf_reader: BufReader<&'a mut TcpStream>,
+}
+
+impl<'a> StreamReader<'a> {
+    pub(crate) fn new(stream: &'a mut TcpStream) -> Self {
+        Self {
+            buf_reader: BufReader::new(stream),
+        }
+    }
+
+    pub(crate) fn get_mut(&mut self) -> &mut TcpStream {
+        self.buf_reader.get_mut()
+    }
+
+    pub(crate) async fn read_resp_value_from_buf_reader(
+        &mut self,
+        request_count: Option<u64>,
+    ) -> Result<Option<RespValue>, Error> {
+        self.read_resp_value(request_count).await
+    }
+
+    async fn read_resp_value(
+        &mut self,
+        request_count: Option<u64>,
+    ) -> Result<Option<RespValue>, Error> {
+        let line = self.read_line_from_tcp_stream(request_count).await?;
+
+        if line.is_empty() {
+            return Ok(None);
+        } else if line.starts_with("+") {
+            return Ok(Some(RespValue::SimpleString(line[1..].trim().to_string())));
+        } else if line.starts_with("$") {
+            let bulk_str_len =
+                usize::from_str_radix(&line[1..].trim(), 10).context("parse-bulk-str-len")?;
+
+            let next_line = self.read_line_from_tcp_stream(request_count).await?;
+
+            if next_line.trim().len() != bulk_str_len {
+                return Err(format!(
+                    "Bulk string len mismatch. Expected {}, got {}. Bulk string: {}",
+                    bulk_str_len,
+                    next_line.len(),
+                    &next_line
+                )
+                .into());
+            }
+
+            return Ok(Some(RespValue::BulkString(next_line.trim().to_string())));
+        } else if line.starts_with("*") {
+            let array_len =
+                usize::from_str_radix(&line[1..].trim(), 10).context("parse-array-len")?;
+            let mut items = vec![];
+
+            for _ in 0..array_len {
+                match Box::pin(self.read_resp_value(request_count)).await? {
+                    Some(item) => items.push(item),
+                    None => return Err("Missing array item".into()),
+                }
+            }
+
+            return Ok(Some(RespValue::Array(items)));
+        } else if line.starts_with(":") {
+            let v = i64::from_str_radix(&line[1..].trim(), 10).context("parse-array-len")?;
+            return Ok(Some(RespValue::Integer(v)));
+        }
+
+        Err(format!("Unexpected incoming RESP string from connection: {}", line).into())
+    }
+
+    async fn read_line_from_tcp_stream(
+        &mut self,
+        request_count: Option<u64>,
+    ) -> Result<String, Error> {
+        let mut buf = String::new();
+        self.buf_reader
+            .read_line(&mut buf)
+            .await
+            .context("number-read")?;
+
+        debug!(
+            "Incoming {} bytes from TcpStream [{}] (RC: {:?})",
+            buf.len(),
+            buf.trim_end(),
+            request_count
+        );
+
+        Ok(buf)
+    }
+
+    pub(crate) async fn read_bulk_bytes_from_tcp_stream(
+        &mut self,
+        request_count: Option<u64>,
+    ) -> Result<Vec<u8>, Error> {
+        let mut size_raw = String::new();
+        self.buf_reader
+            .read_line(&mut size_raw)
+            .await
+            .context("read-bulk-bytes-size")?;
+
+        let size_raw = size_raw.trim_end();
+
+        if !size_raw.starts_with("$") {
+            let next = self.read_line_from_tcp_stream(request_count).await?;
+            return Err(format!(
+                "Invalid start of size for bulk bytes. Got: {} (Next: {}) (RC: {:?})",
+                size_raw, next, request_count
+            )
+            .into());
+        }
+
+        let size = usize::from_str_radix(&size_raw[1..], 10).context("convert-size-to-usize")?;
+
+        let mut buf = Vec::with_capacity(size);
+        buf.resize(size, 0);
+        let read_size = self
+            .buf_reader
+            .read_exact(&mut buf[0..size])
+            .await
+            .context("number-read")?;
+
+        if read_size != size {
+            return Err(
+                format!("Read size mismatch. Read {}. Expected {}.", read_size, size).into(),
+            );
+        }
+
+        debug!(
+            "Incoming {} bytes from TcpStream (RC: {:?})",
+            buf.len(),
+            request_count
+        );
+
+        Ok(buf)
+    }
+}
